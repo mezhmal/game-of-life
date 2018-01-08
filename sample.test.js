b84da8cf8056ec1077d0a25c379044c9cdacffbb
Sergey Mezhmal <s.mezhmal@gmail.com>
2018-01-08 09:55:07 +0300

    Add files via upload

---
+++ sample.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Test suite name', () => {
+    it('should return true', () => {
+        const value = true;
+        const expectedValue = true;
+        assert.equal(value, expectedValue, 'message');
+    });
+});
+
